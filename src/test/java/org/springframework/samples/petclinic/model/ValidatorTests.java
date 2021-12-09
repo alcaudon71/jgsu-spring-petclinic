ba3aab61c96cd2d10a223921f271e7b96645db1f
dragonete <alcaudon71@gmail.com>
2021-12-09 11:00:02 +0100

    nombre vacio

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName(""); // nombre vacio
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
